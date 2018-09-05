8bd321c8774354a411efbb1c5bbef55f227fb030
Cesar Jolibois <diazcd85@gmail.com>
2018-09-05 13:32:24 -0600

    2nd iteration complete

--- test/dock_test.rb
+++ test/dock_test.rb
@@ -10,4 +10,10 @@ class BoatTest < Minitest::Test
     assert_instance_of Dock, dock
   end
 
+  def test_if_it_has_attributes
+    dock = Dock.new("The Rowing Dock", 3)
+
+    assert_equal "The Rowing Dock", dock.name
+    assert_equal 3, dock.max_rental_time
+  end
 end
